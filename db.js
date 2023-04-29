bdeb08a568a7fac831e8d66511f416902aa0c2da
Esteban Donis <estebandonis10@gmail.com>
2023-04-29 16:29:33 -0600

    Datos de base de datos cambiados

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    host: "",
-    user: "",
-    port: 0,
-    password: "",
-    database: ""
+    host: "database-1.cgjrnbwhoqpq.us-east-1.rds.amazonaws.com",
+    user: "postgres",
+    port: 5432,
+    password: "12345678",
+    database: "MedicEZ"
 })
 
 module.exports = pool
