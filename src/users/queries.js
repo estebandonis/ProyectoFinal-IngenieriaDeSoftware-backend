ee84b913350c822e0f4ded620d5e0e6f81519dc4
VIL20776 <vil20776@uvg.edu.gt>
2023-08-10 16:23:43 -0600

    modificación al api

--- src/users/queries.js
+++ src/users/queries.js
@@ -1,5 +1,5 @@
-const AllUsers = "SELECT correo FROM users ORDER BY user_id"
-const ValidateUser = "SELECT user_id FROM users WHERE correo = $1 AND password = $2"
+const AllUsers = "SELECT user_id, correo, tipo, dpi, estado FROM users ORDER BY user_id"
+const ValidateUser = "SELECT user_id, tipo FROM users WHERE correo = $1 AND password = $2"
 const ValidateEmail = "SELECT user_id FROM users WHERE correo = $1"
 const UpdateUserPassword = "UPDATE users SET password = $3 WHERE correo = $1 AND password = $2"
 const AddUser = "INSERT INTO users (correo, password, tipo, estado) VALUES ($1, $2, 'reviewer', 'activo')"
