d23903535866193f3c62f6172b51df3b0ab08f6b
Esteban Donis <estebandonis10@gmail.com>
2023-08-21 22:06:45 -0600

    Agregar reviews implementado

--- src/reviews/queries.js
+++ src/reviews/queries.js
@@ -1,7 +1,9 @@
 const AllReviews = "SELECT * FROM reviews ORDER BY review_id"
-const HospitalReviews = "SELECT correo, rating, comentario FROM reviews INNER JOIN users USING(user_id) WHERE hospital_id = $1"
+const HospitalReviews = "SELECT correo, rating, comentario FROM reviews INNER JOIN users USING(user_id) WHERE hospital_id = $1 ORDER BY review_id"
+const AddUserReview = "INSERT INTO reviews (rating, comentario, user_id, hospital_id) VALUES ($1, $2, (SELECT user_id FROM users WHERE correo = $3), $4)"
 
 module.exports = {
     AllReviews,
     HospitalReviews,
+    AddUserReview,
 }
--- src/reviews/routes.js
+++ src/reviews/routes.js
@@ -6,4 +6,6 @@ const router = Router()
 router.get("/", controller.AllReviews)
 router.get("/getReviewsByHospital/:id", controller.HospitalReviews)
 
+router.post("/addReview/:rating&:comentario&:correo&:id", controller.AddUserReview)
+
 module.exports = router
